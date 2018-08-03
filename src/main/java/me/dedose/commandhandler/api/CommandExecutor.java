ceb624f56a74d3da8c933cb0f52130f77e839df1
dedose1 <ahrasheed2004@gmail.com>
2018-08-02 20:30:33 -0500

    Fixing my terrible code

--- src/main/java/me/dedose/commandhandler/api/CommandExecutor.java
+++ src/main/java/me/dedose/commandhandler/api/CommandExecutor.java
@@ -37,19 +37,19 @@ public class CommandExecutor extends ListenerAdapter {
                                 event.getChannel().sendMessage(cmdHandler.getInsufficientPermissionsMessage()).queue();
                                 return;
                             }
-                            if (args.length >= cmd.getRequiredArgs()) {
-                                if (cmd.getUsableChannels().isEmpty()) {
+                        }
+                        if (args.length >= cmd.getRequiredArgs()) {
+                            if (cmd.getUsableChannels().isEmpty()) {
+                                cmd.getCommandExecuteListener().onCommand(event.getMember(), event.getChannel(), args, event.getMessage());
+                            } else {
+                                if (cmd.getUsableChannels().contains(event.getChannel())) {
                                     cmd.getCommandExecuteListener().onCommand(event.getMember(), event.getChannel(), args, event.getMessage());
                                 } else {
-                                    if (cmd.getUsableChannels().contains(event.getChannel())) {
-                                        cmd.getCommandExecuteListener().onCommand(event.getMember(), event.getChannel(), args, event.getMessage());
-                                    } else {
-                                        event.getChannel().sendMessage(cmdHandler.getIncorrectChannelMessage()).complete().delete().queueAfter(15, TimeUnit.SECONDS);
-                                    }
+                                    event.getChannel().sendMessage(cmdHandler.getIncorrectChannelMessage()).complete().delete().queueAfter(15, TimeUnit.SECONDS);
                                 }
-                            } else {
-                                event.getChannel().sendMessage(cmdHandler.getIncorrectUsageEmbed().getIncorrectEmbed(cmd.getUsage(), event.getMember()).build()).complete().delete().queueAfter(15, TimeUnit.SECONDS);
                             }
+                        } else {
+                            event.getChannel().sendMessage(cmdHandler.getIncorrectUsageEmbed().getIncorrectEmbed(cmd.getUsage(), event.getMember()).build()).complete().delete().queueAfter(15, TimeUnit.SECONDS);
                         }
                     }
                 }
